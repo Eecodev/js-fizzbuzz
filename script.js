a9870af39f2b3360a8324ccbe14923d61c09bf76
Eecodev <mnasser.bbk@gmail.com>
2023-10-11 17:01:24 +0200

    js scaffolding

--- script.js
+++ script.js
@@ -3,3 +3,25 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 
+const wrapperEl = document.getElementById('boxWrapper');
+
+for(let i = 1; i <= 100; i++){
+    console.log(i);
+    let bgBox;
+    if(i % 2 === 0){
+        bgBox = 'even';
+    } else{
+        bgBox = 'odd';
+    }
+    const boxEl = document.createElement('div');
+    boxEl.className = 'box ${bgBox}d-flex justify-content-center align-itames-center';
+    boxEl.innerHTML = i;
+    wrapperEl.append(boxEl);
+}
+
+
+
+// UTILITY
+function randomInteger(min,max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
