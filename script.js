ddd2396cc9f25b0e4e7c000dcc9a77bd6c161cf7
Eecodev <mnasser.bbk@gmail.com>
2023-10-11 18:03:56 +0200

    new conditionals added

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ const wrapperEl = document.getElementById('boxWrapper');
 for(let i = 1; i <= 100; i++){
     console.log(i);
     let bgBox;
+    let content = i;
     if(i % 3 === 0){
         bgBox = 'fizz';
+        content = 'fizz';
     } else if (i % 5 === 0){
         bgBox = 'buzz';
+        content = 'buzz';
     } else
         console.log(i);
 
 
     const boxEl = document.createElement('div');
-    boxEl.className = 'box ${bgBox}d-flex justify-content-center align-itames-center';
+    boxEl.className = 'box ${bgBox} d-flex justify-content-center align-items-center border';
     boxEl.innerHTML = i;
     wrapperEl.append(boxEl);
 }
